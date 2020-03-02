402a7512bc2798e6b4d4e6f3bb1c6904cddac5a4
VICTOR LAWUYI <victor_lawuyi@VICTORs-MacBook-Pro.local>
2020-03-02 20:00:05 +0000

    Update 3

--- src/app/home/notes/edit-notes/edit-notes.page.ts
+++ src/app/home/notes/edit-notes/edit-notes.page.ts
@@ -1,10 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-import { NavController } from '@ionic/angular';
-
-import { HomeService } from '../../home.service';
-import { Homes } from '../../home.model';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-edit-notes',
@@ -12,33 +6,10 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
   styleUrls: ['./edit-notes.page.scss'],
 })
 export class EditNotesPage implements OnInit {
-  loadedclass: Homes[];
-  form: FormGroup;
 
-  constructor(
-    private route: ActivatedRoute,
-    private homeService: HomeService,
-    private navCtrl: NavController
-  ) { }
+  constructor() { }
 
   ngOnInit() {
-    this.route.paramMap.subscribe(paramMap => {
-      if (!paramMap.has('homeId')) {
-        this.navCtrl.navigateBack('/home/tabs/notes');
-        return;
-      }
-      this.loadedclass = this.homeService.getPlace(paramMap.get('homeId'));
-      this.form = new FormGroup({
-        title: new FormControl(this.place.title, {
-          updateOn: 'blur',
-          validators: [Validators.required],
-        }),
-        description: new FormControl(this.place.description, {
-          updateOn: 'blur',
-          validators: [Validators.required, Validators.maxLength(1800)],
-        }),
-      });
-    });
   }
 
 }
